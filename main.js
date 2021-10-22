bfa86d32d500bfc1182eeac453942a6dd66411fa
Maurizio <proiettomaurizio97@gmail.com>
2021-10-22 15:00:14 +0200

    es complete

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let hamburgerMenu = document.querySelector(".hamburger-menu")
 const openHamburgerMenu = document.querySelector(".fas")
 const closeHamburgerMenu = document.querySelector(".hamburger-menu .close")
 
-console.log(closeHamburgerMenu);
 
 openHamburgerMenu.addEventListener("click",
 
@@ -24,3 +23,18 @@ closeHamburgerMenu.addEventListener("click",
 
 )
 
+
+const mediaQuery = window.matchMedia("(min-width: 1000px)")
+
+function handleTabletChange(e) {
+    // Check if the media query is true
+    if (e.matches) {
+      // Then log the following message to the console
+      hamburgerMenu.style.display = "none"
+      console.log('Media Query Matched!')
+    }
+ }
+
+ mediaQuery.addListener(handleTabletChange)
+
+ handleTabletChange(mediaQuery)
