f648b0e0bb9098d9db8f416338303106ec497f40
Maurizio <proiettomaurizio97@gmail.com>
2021-10-22 14:03:49 +0200

    close hamburgher menu

--- main.js
+++ main.js
@@ -4,9 +4,9 @@
 let hamburgerMenu = document.querySelector(".hamburger-menu")
 
 const openHamburgerMenu = document.querySelector(".fas")
-const closeHamburgerMenu
+const closeHamburgerMenu = document.querySelector(".hamburger-menu .close")
 
-console.log(hamburgerMenu, bottonHamburgerMenu);
+console.log(closeHamburgerMenu);
 
 openHamburgerMenu.addEventListener("click",
 
